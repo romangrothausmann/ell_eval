af19c7190668057d8a45615fa4f2714520c9e3fa
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2012-11-28 13:21:16 +0100

    local_sphere_sampling_hist2d_03.m

--- local_sphere_sampling_hist2d.m
+++ local_sphere_sampling_hist2d.m
@@ -2,6 +2,10 @@
 #####sampling only at 2D hist field-points
 ###code taken from octave/scripts/surf_orient_ng_05.m
 ###code regads point weights
+###added test to check if all the surface of the sphere is sampled
+###i.e. if sampling increment and sampling radius lead to sufficient overlap
+###for sphere-sphere intersection radius see: http://mathworld.wolfram.com/Sphere-SphereIntersection.html
+
 
 ######
 #REALLY MAKE SURE TO NOT MIX UP THETA AND PHI!!!!!!!
@@ -32,6 +36,35 @@ lssHist = zeros(nRow,nCol);
 Nx= size(points, 1);
 
 quiet= !verbose;
+
+
+####check if sampling increment and sampling radius lead to sufficient overlap
+
+d= 1;
+R= 1;
+r= sp_lim;
+a= 1/2/d*sqrt((-d+r-R)*(-d-r+R)*(-d+r+R)*(d+r+R)); #http://mathworld.wolfram.com/Sphere-SphereIntersection.html
+
+###law of cosines of angles for a rectangular spherical triangle #http://mathworld.wolfram.com/SphericalTrigonometry.html
+###cos(c)= cos(a)cos(b)
+
+#ca= cos(2*a);
+#cu= cos(2*pi/xBin)*cos(pi/yBin);
+cc= acos(cos(2*pi/xBin)*cos(pi/yBin));
+da= 2*a;
+#cc, da
+
+#if (cu < ca) ##NOTE using cosine invertes relation!!!
+if (cc > da)
+  cc, da
+  printf("WARNING: The chosen sample radius and binning will not lead to a complete covering of the unit sphere!")
+  fflush(stdout);
+  #cc, da #does not output correctly, why???? Has to be before printf! Why???
+  #return
+endif
+
+###check done.
+
 
 if !quiet
   printf("Checking if points lie on sphere...")
@@ -72,7 +105,7 @@ for n=1:1:nRow #phi
     the= (m - 1) * (360 / (nCol - 1)) - 180; #scale range to [-180;180]: theta
 
     #clear xt yt zt;
-    [xt, yt, zt]= sph2cart (the, phi, 1);#projection of 3D points onto unit sphere
+    [xt, yt, zt]= sph2cart (the/180*pi, phi/180*pi, 1);#projection of 3D points onto unit sphere
     xyz= vertcat(xt, yt, zt)';
     
     nnn= sqrt(dot(xyz', xyz')); #norm(c_norm)
